cb7d8959fd9db8b8fe83bba070fcf3cd414f7475
lauro <laulandwalker@gmail.com>
2015-05-10 17:40:33 +0800

    simple todo app

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -4,4 +4,8 @@ from todo_app import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.ListTask.as_view(), name='list-task'),
+                       url(r'^create/$', views.CreateNewTask.as_view(), name='create-task'),
+                       url(r'^create/$', views.CreateNewTask.as_view(), name='create-task'),
+                       url(r'^delete/(?P<pk>\d+)/$', views.DeleteTask.as_view(), name='delete-task'),
+                       url(r'^update/(?P<pk>\d+)/$', views.UpdateTask.as_view(), name='update-task'),
                        )
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from todo_app.models import TaskModel
 
 
@@ -8,3 +8,23 @@ class ListTask(ListView):
     model = TaskModel
     template_name = 'todo_app/task-list.html'
     context_object_name = 'tasks'
+
+
+class CreateNewTask(CreateView):
+    model = TaskModel
+    template_name = 'todo_app/task-create.html'
+    success_url = '/todo/'
+    form_class = TaskForm
+
+
+class DeleteTask(DeleteView):
+    model = TaskModel
+    template_name = 'todo_app/task-delete.html'
+    success_url = '/todo/'
+
+
+class UpdateTask(UpdateView):
+    model = TaskModel
+    template_name = 'todo_app/task-update.html'
+    form_class = TaskForm
+    success_url = '/todo/'
