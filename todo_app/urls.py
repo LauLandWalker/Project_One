8eac706431469abe551f59d7b682147a9847e886
lauro <laulandwalker@gmail.com>
2015-05-10 17:03:53 +0800

    simple todo app

---
+++ todo_app/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from todo_app import views
+
+
+urlpatterns = patterns('',
+                       url(r'^$', views.index, name='index'),
+
+                       )
